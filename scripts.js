8dfae653ba6a479311eef20138118820d3dfa09d
Martin Ivan Rios <martin.i@geopagos.com>
2021-02-17 20:42:42 -0300

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,14 @@
+var nameInput = prompt("Ingrese su nombre: ");
+
+console.log("Hola " + nameInput + "!");
+
+
+var floatInput = parseFloat(prompt("Ingrese un numero: "));
+var savedNumber = 8;
+
+console.log(savedNumber + " + " + floatInput + " = " + (savedNumber+floatInput));
+
+
+var textInput = prompt("Ingrese cualquier texto: ");
+
+alert("Texto ingresado: " + textInput);
