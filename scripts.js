ca0cee445c657ae922fd1e6fd7a68001f64eaf3e
Martin Ivan Rios <martin.i@geopagos.com>
2021-02-18 10:46:04 -0300

    Desafio 4

--- scripts.js
+++ scripts.js
@@ -1,23 +1,99 @@
-const dias = ["Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo",];
+var items = [];
+var withIVA = false;
 
-for (var i in dias) {
-    if (i === "6") {
-        alert("Dia 7 encontrado!");
-    }
-    
-    if (i%2 === 1) {
-        console.log("El dia " + dias[i] + " es par");
+alert("Calculador de total de carrito de compras")
+while(true) {
+    let input = prompt("Ingrese una opcion: crear-carrito, sumar-iva, mas-caro, mas-barato, total, limpiar-carrito")
+
+    switch(input) {
+        case "crear-carrito":
+            createCart();
+            break;
+        case "sumar-iva":
+            addIVA();
+            break;
+        case "mas-caro":
+            pricierItem();
+            break;
+        case "mas-barato":
+            cheaperItem();
+            break;
+        case "total":
+            totalSum();
+            break;
+        case "limpiar-carrito":
+            resetItems();
+            break;
+        default:
+            alert("Por favor, elegir una de las opciones disponibles");
     }
 }
 
-/* Forma 2
-for (var i = 0; i < dias.length; i++) {
-    if (i === 6) {
-        alert("Dia 7 encontrado!");
-    }
-    
-    if (i%2 === 1) {
-        console.log("El dia " + dias[i] + " es par");
+function createCart() {
+    while(true) {
+        let input = prompt("Ingrese el costo de un item (o cualquier letra para finalizar)");
+        if (!isNumber(input)) {
+            break;
+        } else {
+            items = [];
+            withIVA = false;
+            
+            items.push(parseFloat(input));
+        }
     }
 }
-*/
+
+function addIVA() {
+    if (withIVA === true) {
+        alert("IVA ya añadido");
+        return;
+    } else if (items.length > 0) {
+        withIVA = true;
+        for (var i in items) {
+            items[i] = items[i]*1.21;
+        }
+    }
+}
+
+function pricierItem() {
+    let pricier;
+    for (var item of items) {
+        if (pricier === undefined || item > pricier) {
+            pricier = item;
+        }
+    }
+
+    alert("El item mas caro sale: " + pricier);
+}
+
+function cheaperItem() {
+    let cheaper;
+    for (var item of items) {
+        if (cheaper === undefined || item < cheaper) {
+            cheaper = item;
+        }
+    }
+
+    alert("El item mas barato sale: " + cheaper);
+}
+
+function totalSum() {
+    let total = 0;
+    for (var item of items) {
+        total += item;
+    }
+
+    alert("El total es: " + total);
+}
+
+function resetItems() {
+    items = [];
+    withIVA = false;
+}
+
+function isNumber(str) {
+    if (typeof str != "string") {
+        return false;
+    }
+    return !isNaN(str) && !isNaN(parseFloat(str));
+}
